ca0351a61a8280f16379aff902536d2a0fabb7ab
Aral Balkan (LabTop) <mail+elementary-labtop@ar.al>
2021-08-17 09:07:37 +0100

    Update test script to launch apps from Odin via flatpak

--- test.sh
+++ test.sh
@@ -9,5 +9,5 @@ G_MESSAGES_DEBUG=all gala -d :3 &
 sleep 2
 
 # start a few applications to switch between
-DISPLAY=:3 xterm &
-DISPLAY=:3 io.elementary.calculator
+DISPLAY=:3 flatpak run io.elementary.calculator &
+DISPLAY=:3 flatpak run io.elementary.tasks
